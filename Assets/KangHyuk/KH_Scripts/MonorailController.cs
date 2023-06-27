f3eba5606493c4432b8459e03a5b876426b78d62
ssamjol <ssamjol1788@naver.com>
2023-06-27 16:18:27 +0900

    모노레일 사운드_0627

--- Assets/KangHyuk/KH_Scripts/MonorailController.cs
+++ Assets/KangHyuk/KH_Scripts/MonorailController.cs
@@ -13,14 +13,31 @@ public class MonorailController : MonoBehaviour
     public Animator monorailAnim;
     public Animator doorAnim;
 
+    private AudioSource audioSource;
+    public AudioClip dingdong;
+    public AudioClip monorailMove;
+    public AudioClip doorOpen;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void doorSound()
+    {
+        AudioSource.PlayClipAtPoint(doorOpen, transform.position);
+    }
     public void MoveToWaypointIndex(int waypointIndex)
     {
         if (waypointIndex >= 0 && waypointIndex < waypoints.Count)
         {
             currentWaypointIndex = waypointIndex;
             doorAnim.SetBool("isOpen", false);
+            doorSound();
             isMoving = true;
+            StartMovingSound();
             monorailAnim.SetBool("IsMoving", true);
+            
         }
         else
         {
@@ -37,11 +54,28 @@ public class MonorailController : MonoBehaviour
         }
 
         doorAnim.SetBool("isOpen", true);
+        doorSound();
     }
 
     public void CloseDoor()
     {
         doorAnim.SetBool("isOpen", false);
+        doorSound();
+    }
+
+    public void StartMovingSound()
+    {
+        // 소리 재생하기
+        audioSource.clip = monorailMove;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+
+    public void StopMovingSound()
+    {
+        // 소리 중지하기
+        audioSource.Stop();
+
     }
 
     private void Update()
@@ -50,15 +84,19 @@ public class MonorailController : MonoBehaviour
         {
             // 현재 웨이포인트
             Vector3 currentWaypointPosition = waypoints[currentWaypointIndex].position;
+            audioSource.transform.position = transform.position;
 
             // 웨이포인트에 도착한 경우 이동 중지
             if (Vector3.Distance(transform.position, currentWaypointPosition) < 0.1f)
             {
                 isMoving = false;
+                StopMovingSound();
+                AudioSource.PlayClipAtPoint(dingdong, transform.position);
                 monorailAnim.SetBool("IsMoving", false);
                 doorAnim.SetBool("isOpen", true);
                 Debug.Log("도착!");
-                Invoke("CloseDoor", 5f);
+                Invoke("CloseDoor", 4f);
+                Invoke("doorSound", 4f);
             }
             else
             {
