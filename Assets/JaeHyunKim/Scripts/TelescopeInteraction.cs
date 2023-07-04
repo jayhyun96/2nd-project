36171822e869bd590522be43364026ccba2af39d
KSH7233 <55950694+KSH7233@users.noreply.github.com>
2023-07-04 16:40:50 +0900

    망원경, 타임어택, 모노레일 스탬프에서 함수호출
    stampClear 호출

--- Assets/JaeHyunKim/Scripts/TelescopeInteraction.cs
+++ Assets/JaeHyunKim/Scripts/TelescopeInteraction.cs
@@ -30,6 +30,8 @@ public class TelescopeInteraction : XRSimpleInteractable
         // 다른 환경 설정들...
 
         isTelescopeView = true;
+
+        GameManager.Instance.StampClear((int)STAMP.OBSERVATION);
     }
 
     public void ExitTelescopeView()
